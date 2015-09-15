ae11d0088305d411c37ca2c728a2ad51e76d5b94
Benoît Laniel <nels+git@pgroupe.net>
2015-09-15 17:07:01 +0000

    create odin flashable recovery automatically

--- custombootimg.mk
+++ custombootimg.mk
@@ -1,3 +1,6 @@
+TWRP_VERSION := $(shell cat bootable/recovery/variables.h | grep TW_VERSION_STR | cut -d\" -f2)
+TWRP_NAME := twrp-$(TWRP_VERSION)-$(TARGET_DEVICE)
+
 $(INSTALLED_RECOVERYIMAGE_TARGET): $(MKBOOTIMG) \
 		$(recovery_ramdisk) \
 		$(recovery_kernel)
@@ -7,3 +10,8 @@ $(INSTALLED_RECOVERYIMAGE_TARGET): $(MKBOOTIMG) \
 	$(hide) $(MKBOOTIMG) $(INTERNAL_RECOVERYIMAGE_ARGS) $(BOARD_MKBOOTIMG_ARGS) --output $@
 	$(hide) $(call assert-max-image-size,$@,$(BOARD_RECOVERYIMAGE_PARTITION_SIZE))
 	@echo -e ${PRT_IMG}"----- Made recovery image: $@ --------"${CL_RST}
+	@echo -e ${PRT_IMG}"----- Making odin recovery image ------"${CL_RST}
+	cd $(PRODUCT_OUT) && tar -H ustar -c $(shell basename $@) > $(TWRP_NAME).tar
+	cd $(PRODUCT_OUT) && md5sum -t $(TWRP_NAME).tar >> $(TWRP_NAME).tar
+	mv $(PRODUCT_OUT)/$(TWRP_NAME).tar $(PRODUCT_OUT)/$(TWRP_NAME).tar.md5
+	@echo -e ${PRT_IMG}"----- Made odin recovery image: $(PRODUCT_OUT)/$(TWRP_NAME).tar.md5 --------"${CL_RST}
