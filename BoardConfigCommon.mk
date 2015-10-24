3241cd49eda12d7994a1d6eec41d27b8ab3d02de
Benoît Laniel <nels+git@pgroupe.net>
2015-10-24 19:26:24 +0200

    first import
    The following files come from stock rom:
    sec_keyboard.idc, sec_key.kl and sec_touchscreen.kl
    
    Patches are needed for old binaries support.
    
    Depends on (bionic):
    5cc8fcb Revert "Remove text-relocs support for apps targeting M+"
    
    Depends on (frameworks/native):
    293d0d5 Revert "Remove Parcel::writeIntPtr."

---
+++ BoardConfigCommon.mk
@@ -0,0 +1,43 @@
+#
+# Copyright 2013 The Android Open-Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Architecture
+TARGET_ARCH := x86
+TARGET_ARCH_VARIANT := atom
+TARGET_CPU_ABI := x86
+
+TARGET_BOARD_PLATFORM := clovertrail
+TARGET_BOOTLOADER_BOARD_NAME := clovertrail
+
+# Inline kernel building
+TARGET_KERNEL_SOURCE := kernel/samsung/santos10
+TARGET_KERNEL_CONFIG := cyanogenmod_santos10_defconfig
+
+KERNEL_TOOLCHAIN_PREFIX := x86_64-linux-android-
+BOARD_KERNEL_IMAGE_NAME := bzImage
+
+# Filesystem
+TARGET_USERIMAGES_USE_EXT4 := true
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 2474639360
+BOARD_FLASH_BLOCK_SIZE := 1024
+
+# OpenGL
+USE_OPENGL_RENDERER := true
+TARGET_RUNNING_WITHOUT_SYNC_FRAMEWORK := true
+TARGET_REQUIRES_SYNCHRONOUS_SETSURFACE := true
+
+# Recovery
+TARGET_RECOVERY_FSTAB := device/samsung/santos10-common/rootdir/fstab.santos10
