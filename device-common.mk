31b9212db0c55d75c7c67e1760c873c00ad52613
Benoît Laniel <nels+git@pgroupe.net>
2015-10-25 00:53:50 +0200

    device: add media profiles

--- device-common.mk
+++ device-common.mk
@@ -44,6 +44,13 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/tinyucm.conf:system/etc/tinyucm.conf \
     $(LOCAL_PATH)/configs/audio_policy.conf:system/etc/audio_policy.conf
 
+# Multimedia
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/media_codecs.xml:system/etc/media_codecs.xml \
+    frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:system/etc/media_codecs_google_audio.xml \
+    frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:system/etc/media_codecs_google_telephony.xml \
+    frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:system/etc/media_codecs_google_video.xml
+
 # Wifi
 PRODUCT_PACKAGES += \
     dhcpcd.conf \
