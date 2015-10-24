3a48b5fd3dbeba6d9e7d29d04ef0e1ac9b1d7c9c
Benoît Laniel <nels+git@pgroupe.net>
2015-10-24 23:54:10 +0200

    device: add usb host feature

--- device-common.mk
+++ device-common.mk
@@ -47,7 +47,8 @@ PRODUCT_COPY_FILES += \
 # These are the hardware-specific features
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/tablet_core_hardware.xml:system/etc/permissions/tablet_core_hardware.xml \
-    frameworks/native/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:system/etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml
+    frameworks/native/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:system/etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml \
+    frameworks/native/data/etc/android.hardware.usb.host.xml:system/etc/permissions/android.hardware.usb.host.xml
 
 PRODUCT_CHARACTERISTICS := tablet
 
