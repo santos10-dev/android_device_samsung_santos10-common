7f22decd6f84ea500c54ded5ef8daf6bdd18b3a0
Benoît Laniel <nels+git@pgroupe.net>
2015-10-25 00:11:42 +0200

    add wifi tethering support

--- device-common.mk
+++ device-common.mk
@@ -35,7 +35,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PACKAGES += \
     dhcpcd.conf \
     wpa_supplicant \
-    wpa_supplicant.conf
+    wpa_supplicant.conf \
+    hostapd
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/wpa_supplicant_overlay.conf:system/etc/wifi/wpa_supplicant_overlay.conf
