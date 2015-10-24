71583158eacdec04920a56c5763b8ca55ca8f90d
Benoît Laniel <nels+git@pgroupe.net>
2015-10-25 00:18:01 +0200

    add bluetooth
    Configuration file is a copy of vnd_fugu.txt
    
    Patch is needed to specify bluetooth configuration.
    
    Depends on (hardware/broadcom/libbt):
    31a5cbd libbt: Add support for board-specific configuration

--- device-common.mk
+++ device-common.mk
@@ -66,7 +66,8 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:system/etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml \
     frameworks/native/data/etc/android.hardware.usb.host.xml:system/etc/permissions/android.hardware.usb.host.xml \
     frameworks/native/data/etc/android.hardware.wifi.xml:system/etc/permissions/android.hardware.wifi.xml \
-    frameworks/native/data/etc/android.hardware.wifi.direct.xml:system/etc/permissions/android.hardware.wifi.direct.xml
+    frameworks/native/data/etc/android.hardware.wifi.direct.xml:system/etc/permissions/android.hardware.wifi.direct.xml \
+    frameworks/native/data/etc/android.hardware.bluetooth.xml:system/etc/permissions/android.hardware.bluetooth.xml
 
 PRODUCT_CHARACTERISTICS := tablet
 
