d325950e01e4f86f1b5bd49ce797c4aa52649aea
Benoît Laniel <nels+git@pgroupe.net>
2015-11-04 21:21:44 +0100

    add camera
    The following files come from stock rom:
    camera_profiles.xml and media_profiles.xml
    
    There is a bug in antibanding handling, use wrapper as a workaround.
    Move isp_fr.bin to /system/etc/firmware, path is hardcoded in HAL.
    
    libshim_camera is based on Cristopher R. Palmer's work

--- device-common.mk
+++ device-common.mk
@@ -77,6 +77,16 @@ PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:system/etc/media_codecs_google_video.xml \
     $(LOCAL_PATH)/configs/wrs_omxil_components.list:system/etc/wrs_omxil_components.list
 
+# Camera
+PRODUCT_PACKAGES += \
+    libshim_camera \
+    \
+    camera.santos10
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/camera_profiles.xml:system/etc/camera_profiles.xml \
+    $(LOCAL_PATH)/configs/media_profiles.xml:system/etc/media_profiles.xml
+
 # Power
 PRODUCT_PACKAGES += \
     power.santos10
