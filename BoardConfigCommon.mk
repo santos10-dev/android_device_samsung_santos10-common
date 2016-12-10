3397b494401e5a7effd5d94c2e4a499390cc2b45
Benoît Laniel <nels+git@pgroupe.net>
2016-12-10 20:55:37 +0100

    Revert "board: enable houdini enhancements by intel"
    This reverts commit a325b3a59bbc80451d7a91021a49a0e1aa9a7a44.

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -30,8 +30,6 @@ MALLOC_SVELTE := true
 TARGET_CPU_ABI2 := armeabi-v7a
 TARGET_CPU_ABI_LIST_32_BIT := x86,armeabi-v7a,armeabi
 BUILD_ARM_FOR_X86 := true
-PRC_COMPATIBILITY_PACKAGE := true
-COMPATIBILITY_ENHANCEMENT_PACKAGE := true
 
 # Specific headers
 TARGET_BOARD_KERNEL_HEADERS := device/samsung/santos10-common/kernel-headers
