47dcbcaba001ecc9e32e00ecb3269159da1c3761
Benoît Laniel <nels+git@pgroupe.net>
2016-12-18 18:33:28 +0100

    bluetooth: kernel does not have support for CLOCK_BOOTTIME_ALARM
    Change-Id: Ifeb8a5db2cb2f13c2f9299e6eec577cff89d30e1

--- bluetooth/bdroid_buildcfg.h
+++ bluetooth/bdroid_buildcfg.h
@@ -19,4 +19,7 @@
 
 #define BTM_DEF_LOCAL_NAME "Samsung Galaxy Tab 3"
 
+/* Defined if the kernel does not have support for CLOCK_BOOTTIME_ALARM */
+#define KERNEL_MISSING_CLOCK_BOOTTIME_ALARM TRUE
+
 #endif
