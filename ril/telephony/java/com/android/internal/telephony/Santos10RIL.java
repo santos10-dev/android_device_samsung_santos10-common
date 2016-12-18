4d85dd066ce2dd2809297c09c3f72d8cb99df7e8
Benoît Laniel <nels+git@pgroupe.net>
2016-12-18 10:35:02 +0100

    ril: update to N

--- ril/telephony/java/com/android/internal/telephony/Santos10RIL.java
+++ ril/telephony/java/com/android/internal/telephony/Santos10RIL.java
@@ -26,6 +26,7 @@ import android.telephony.PhoneNumberUtils;
 
 import com.android.internal.telephony.uicc.IccCardApplicationStatus;
 import com.android.internal.telephony.uicc.IccCardStatus;
+import com.android.internal.telephony.uicc.IccUtils;
 
 import java.util.ArrayList;
 import java.util.Collections;
